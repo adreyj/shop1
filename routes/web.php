3b65f8c4c9fb509282dab020c6ef14481fcf7ea5
ar rafallo <arafallo@gbox.adnu.edu.ph>
2023-04-19 03:03:00 +0800

    Second Commit

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/shop', [App\Http\Controllers\ProductController::class, 'index'])->name('shop');
+
+
+
